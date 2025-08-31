45e6bfd0460f9d20c926d8d7df56881fa65ce827
Cyborgnetical <140975796+Cyborgnetical@users.noreply.github.com>
2025-08-31 00:28:35 -0700

    wegwegwegewg ewGSgwegwg

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    // its just the way gitlab works.
+    outDir: 'public',
+    publicDir: 'static',
+});
