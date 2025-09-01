32b08da853e77dba32b691bcf4095e51e7a27340
Cyborgnetical <140975796+Cyborgnetical@users.noreply.github.com>
2025-09-01 00:00:41 -0700

    ta da

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-    // its just the way gitlab works.
-    outDir: 'public',
-    publicDir: 'static',
+  // its just the way gitlab works.
+  outDir: 'public',
+
+  publicDir: 'static',
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
